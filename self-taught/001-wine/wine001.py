d71745c5cf8f658b7990618dab54630e6262e94a
Rockyfu <453618045@qq.com>
2022-01-01 18:27:43 +0800

    Update wine001.py

--- self-taught/001-wine/wine001.py
+++ self-taught/001-wine/wine001.py
@@ -1,1 +1,15 @@
 
+
+from sklearn.datasets import load_wine
+
+#从skearn的datasets模块中载入数据集
+wine_dataset = load_wine()
+
+print('\n\n\n')
+print("========================================")
+print("红酒数据集中的键: \n{}".format(wine_dataset.keys()))
+print("========================================")
+print("红酒数据集中的键: \n{}".format(wine_dataset['data'].shape))
+print("========================================")
+print("答应简短描述: \n{}".format(wine_dataset['DESCR']))
+print("========================================")
