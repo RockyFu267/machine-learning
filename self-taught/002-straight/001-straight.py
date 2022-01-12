06c39bd76a63d37b3baa2787c484712d7323581d
Rockyfu <453618045@qq.com>
2022-01-12 23:56:11 +0800

    Update 001-straight.py

--- self-taught/002-straight/001-straight.py
+++ self-taught/002-straight/001-straight.py
@@ -1,1 +1,16 @@
 
+# %matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+
+#令x为-5 5 之间，元素为100的等差数列
+X = np.linspace(-5,5,100)
+
+#输入直线方程
+y = 0.5 * X +3
+
+plt.plot(X,y,c='orange')
+
+#标题设置
+plt.title("Straight Line")
+plt.show()
